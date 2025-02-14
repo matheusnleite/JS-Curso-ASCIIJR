da8a95ce3a04df633857197e081943b27928f934
Matheus <matheusn06leite@gmail.com>
2025-02-14 11:27:24 -0300

    Aprendendo jeitos de colocar o js na nossa pagina e aprendendo tipos de saida

---
+++ js/app.js
@@ -0,0 +1,3 @@
+document.getElementById('texto').innerHTML = 'Ola Mundo!'; //Colocando um texto dentro do paragrafo com id TEXTO
+alert(10 + 10); //Mostrando um alerta com a soma de 10 + 10
+console.log('Oi isso é um console.log');
