ca2fbce0d50f7b687c0e09321d79e79f33d46564
Matheus <matheusn06leite@gmail.com>
2025-02-17 14:53:36 -0300

    aprendendo var, let, const e objetos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
-document.getElementById('texto').innerHTML = 'Ola Mundo!'; //Colocando um texto dentro do paragrafo com id TEXTO
-alert(10 + 10); //Mostrando um alerta com a soma de 10 + 10
-console.log('Oi isso é um console.log');
+//VAR X LET X CONST
+
+//VAR = pode mudar o valor, funciona dentro de qualquer escopo
+
+//LET = variavel unica, nao pode ser redeclarada, so funciona dentro do escopo que esta trabalhando
+
+//CONST = nao pode mudar o valor, nao pode ser redeclarada
+
+/*  OBJETOS
+2 Objetos são basicamente variáveis com muitos valores dentro.
+3
+4 Ex: const carro = {marca: "ford", modelo:"ka", ano: 2015}
+5
+6 Os valores dentro de um objeto são chamados propriedades.
+7
+8 Objetos também podem ter métodos. Um Método é uma função colocadas dentro 9 de uma propriedade. */
+
+const carro = {
+  marca: "ford",
+  modelo: "ka",
+  ano: 2015,
+  placa: "ABC-1234",
+  buzina: function () {
+    alert("BIIIIIIIIIIIIIIIIII");
+  },
+  completo: function () {
+    return "A marca é " + this.marca + " e o modelo é " + this.modelo;
+  },
+};
+
+console.log(carro.completo());
